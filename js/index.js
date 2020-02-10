896f7cedb299852a514a02e44efe0f99dceaaa7a
Mariusz Czechowski <mczechowski@opidomain.local>
2020-02-10 11:37:56 +0100

    centering position of name and location

--- js/index.js
+++ js/index.js
@@ -24,6 +24,8 @@ elements.hamburger1.addEventListener('click', () => {
         for (const e of elements.contactsList.children) { // DOM traversing
             e.classList.remove('density');
             e.children[1].classList.remove('density');
+            e.children[0].children[0].classList.add('name')
+            e.children[0].children[2].classList.add('location')
             // console.log(e)
         }
         state = 0;
@@ -38,7 +40,9 @@ elements.hamburger2.addEventListener('click', () => {
         for (const e of elements.contactsList.children) { // DOM traversing
             e.classList.add('density');
             e.children[1].classList.add('density');
-            // console.log(e.children[1]);
+            e.children[0].children[0].classList.remove('name')
+            e.children[0].children[2].classList.remove('location')
+            //console.log(e.children[0].children);
         }
         state = 1;
         console.log(state)
@@ -59,7 +63,7 @@ elements.contactsList.addEventListener('click', e => {
 //---------------------------- get data contacts ------------------------
 async function getData() {
     try {
-        const result = await fetch('/data/contacts.json');
+        const result = await fetch('/data/testContacts.json');
         const resultJson = await result.json();
         console.log(resultJson);
         return (resultJson);
@@ -79,9 +83,8 @@ async function getData() {
 
 let contacts;
 getData().then(data => {
-    contacts = data
+    contacts = data;
     const markup = contacts.map(person => {
-        //console.log(person);
         return `
             <div class="contact__item">
                 <ul class="contact__item--details">
@@ -91,10 +94,10 @@ getData().then(data => {
                     <li class='phone'>${person.phone}</li>
                 </ul>
                 <ul class="contact__item--details sub-list">
-                       <li class="title">${person.title}</li>
-                       <li class="group">${person.group}</li>
-                       <li class="other">${person.other}</li>
-                       <li class="secondPhone">${person.secondPhone}</li>
+                    <li class="title"></li>
+                    <li class="group">${person.group}</li>
+                    <li></li>
+                    <li class="secondPhone">${person.secondPhone}</li>
                 </ul>
             </div>  
         `;
@@ -112,7 +115,7 @@ const clearContactsList = () => {
 const findMatches = (wordToMatch, contacts) => {
     return contacts.filter(person => {
         const regex = new RegExp(wordToMatch, 'gi');
-        return person.name.match(regex) || person.departament.match(regex) || person.phone.match(regex) || person.secondPhone.match(regex)
+        return person.name.match(regex) || person.location.match(regex) || person.phone.match(regex) || person.secondPhone.match(regex)
     });
 }
 
@@ -124,21 +127,21 @@ function displayMatches() {
     const markup = matchResult.map(person => {
         const regex = RegExp(this.value, 'gi');
         const personName = person.name.replace(regex, `<span class="hl">${this.value}</span>`);
-        const personDepartament = person.departament.replace(regex, `<span class="hl uppercase">${this.value}</span>`);
+        const personLocation = person.location.replace(regex, `<span class="hl uppercase">${this.value}</span>`);
         const personPhone = person.phone.replace(regex, `<span class="hl">${this.value}</span>`);
         const personSecondPhone = person.secondPhone.replace(regex, `<span class="hl">${this.value}</span>`);
         return `
         <div class="contact__item ${state == 1 ? 'density' : ''}">
             <ul class="contact__item--details">
                 <li class='name'>${personName}</li>
-                <li class='departament'>${personDepartament}</li>
-                <li class='title'>${person.location}</li>
+                <li class='departament'>${person.departament}</li>
+                <li class='location'>${personLocation}</li>
                 <li class='phone'>${personPhone}</li>
             </ul>
             <ul class="contact__item--details sub-list ${state == 1 ? 'density' : ''}">
-                <li class="title">${person.title}</li>
+                <li class="title"></li>
                 <li class="group">${person.group}</li>
-                <li class="other">${person.other}</li>
+                <li></li>
                 <li class="secondPhone">${personSecondPhone}</li>
             </ul>
         </div>  
